eb8246960b06cd6c0c2879dc91bcf6937a8f2c22
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-22 12:15:45 -0300

    feat: basic prints

---
+++ java-fundamentals/02-classes-and-data-types/basics/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("First line");
+        System.out.print("Second line");
+        System.out.print(" Also in second line");
+
+    }
+}
