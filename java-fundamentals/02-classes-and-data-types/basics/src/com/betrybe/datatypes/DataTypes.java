8cb17296d4f55de75424cf3ab8d3d8413d12ac96
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-22 12:34:15 -0300

    feat: creating first variable

---
+++ java-fundamentals/02-classes-and-data-types/basics/src/com/betrybe/datatypes/DataTypes.java
@@ -0,0 +1,11 @@
+package com.betrybe.datatypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int answerToEverything = 42;
+
+        System.out.println("The answer to everythin is everything");
+        System.out.println(answerToEverything);
+    }
+    
+}
