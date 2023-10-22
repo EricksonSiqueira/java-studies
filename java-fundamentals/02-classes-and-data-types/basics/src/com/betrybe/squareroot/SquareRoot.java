24552098c57b1c3c512302fd8db8672ebc694ee3
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-22 12:30:54 -0300

    feat: using java Math package

---
+++ java-fundamentals/02-classes-and-data-types/basics/src/com/betrybe/squareroot/SquareRoot.java
@@ -0,0 +1,8 @@
+package com.betrybe.squareroot;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        System.out.println("The square root of 25 is:");
+        System.out.println(Math.sqrt(25));
+    }
+}
